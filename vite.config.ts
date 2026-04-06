507fd57dda3debdd9592c261ac238339076f65d1
MohammadHejawi <69235810+MohammadHejawi@users.noreply.github.com>
2026-04-06 13:06:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: "/",
@@ -17,3 +20,4 @@ export default defineConfig({
     emptyOutDir: true,
   },
 });
+
